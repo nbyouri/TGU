33f30e3126fa714841624233d18270f76f31f941
antoinerime <antoine.rime@student.uclouvain.be>
2017-02-28 12:11:05 +0100

    Added class event

---
+++ app/src/main/java/muga/thegreatuniversity/lists/EventType.java
@@ -0,0 +1,12 @@
+package muga.thegreatuniversity.lists;
+
+/**
+ * Created on 28/02/17 .
+ * Authors : Rime Antoine, Moers Tristan, Mouton Youri, Voet Rémy
+ * Muga Copyright
+ */
+
+public enum EventType {
+    DETEV, //Determinist event
+    NODETEV, //Not determinist event
+}
