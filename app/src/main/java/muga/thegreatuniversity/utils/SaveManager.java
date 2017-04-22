c447f637ba59b5fce949a732a561ac9a4ef94fe6
Youri Mouton <youri@NetBSD.org>
2017-04-22 15:47:17 +0200

    Convert dimension units to px.

--- app/src/main/java/muga/thegreatuniversity/utils/SaveManager.java
+++ app/src/main/java/muga/thegreatuniversity/utils/SaveManager.java
@@ -61,33 +61,23 @@ public class SaveManager {
         }
     }
 
-    public static boolean loadUniversity(Context context){
-        try {
-            InputStream inputStream = context.openFileInput(UNIVERSITY_FILE);
-
+    public static boolean loadUniversity(Context context) throws Exception {
+        InputStream inputStream = context.openFileInput(UNIVERSITY_FILE);
             if (inputStream != null) {
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                String receiveString = "";
-                StringBuilder stringBuilder = new StringBuilder();
+        if (inputStream != null) {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String receiveString = "";
+            StringBuilder stringBuilder = new StringBuilder();
 
-                while ((receiveString = bufferedReader.readLine()) != null) {
-                    stringBuilder.append(receiveString);
-                }
-
-                inputStream.close();
-                try {
-                    String save = stringBuilder.toString();
-                    University.get().loadJSON(new JSONObject(save));
-                    Logger.info("Save read : "+save);
-                    return true;
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
+            while ((receiveString = bufferedReader.readLine()) != null) {
+                stringBuilder.append(receiveString);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+            inputStream.close();
+            String save = stringBuilder.toString();
+            University.get().loadJSON(new JSONObject(save));
+            return true;
         return false;
     }
 
